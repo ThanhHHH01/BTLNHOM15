c1d8c741f1a115ae76c062b9b36eb99727d1821b
ThanhHHH01 <112218811+ThanhHHH01@users.noreply.github.com>
2023-05-22 00:54:33 +0700

    LQT

---
+++ Controllers/HoiVienController.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BTLNHOM15.Models;
+
+namespace BTLNHOM15.Controllers
+{
+    public class HoiVienController : Controller
+    {
+        private readonly MvcGymContext _context;
+
+        public HoiVienController(MvcGymContext context)
+        {
+            _context = context;
+        }
+
+        // GET: HoiVien
+        public async Task<IActionResult> Index()
+        {
+            var mvcGymContext = _context.HoiVien.Include(h => h.GoiTap);
+            return View(await mvcGymContext.ToListAsync());
+        }
+
+        // GET: HoiVien/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.HoiVien == null)
+            {
+                return NotFound();
+            }
+
+            var hoiVien = await _context.HoiVien
+                .Include(h => h.GoiTap)
+                .FirstOrDefaultAsync(m => m.HoiVienID == id);
+            if (hoiVien == null)
+            {
+                return NotFound();
+            }
+
+            return View(hoiVien);
+        }
+
+        // GET: HoiVien/Create
+        public IActionResult Create()
+        {
+            ViewData["MaGoiTap"] = new SelectList(_context.Set<GoiTap>(), "MaGoiTap", "TenGoi");
+            return View();
+        }
+
+        // POST: HoiVien/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("HoiVienID,TenHV,Address,SĐT,EmailHV,MaGoiTap,Ngaybatdau,Ngayketthuc")] HoiVien hoiVien)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hoiVien);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaGoiTap"] = new SelectList(_context.Set<GoiTap>(), "MaGoiTap", "TenGoi", hoiVien.MaGoiTap);
+            return View(hoiVien);
+        }
+
+        // GET: HoiVien/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.HoiVien == null)
+            {
+                return NotFound();
+            }
+
+            var hoiVien = await _context.HoiVien.FindAsync(id);
+            if (hoiVien == null)
+            {
+                return NotFound();
+            }
+            ViewData["MaGoiTap"] = new SelectList(_context.Set<GoiTap>(), "MaGoiTap", "TenGoi", hoiVien.MaGoiTap);
+            return View(hoiVien);
+        }
+
+        // POST: HoiVien/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("HoiVienID,TenHV,Address,SĐT,EmailHV,MaGoiTap,Ngaybatdau,Ngayketthuc")] HoiVien hoiVien)
+        {
+            if (id != hoiVien.HoiVienID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(hoiVien);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HoiVienExists(hoiVien.HoiVienID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaGoiTap"] = new SelectList(_context.Set<GoiTap>(), "MaGoiTap", "TenGoi", hoiVien.MaGoiTap);
+            return View(hoiVien);
+        }
+
+        // GET: HoiVien/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.HoiVien == null)
+            {
+                return NotFound();
+            }
+
+            var hoiVien = await _context.HoiVien
+                .Include(h => h.GoiTap)
+                .FirstOrDefaultAsync(m => m.HoiVienID == id);
+            if (hoiVien == null)
+            {
+                return NotFound();
+            }
+
+            return View(hoiVien);
+        }
+
+        // POST: HoiVien/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.HoiVien == null)
+            {
+                return Problem("Entity set 'MvcGymContext.HoiVien'  is null.");
+            }
+            var hoiVien = await _context.HoiVien.FindAsync(id);
+            if (hoiVien != null)
+            {
+                _context.HoiVien.Remove(hoiVien);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HoiVienExists(string id)
+        {
+          return (_context.HoiVien?.Any(e => e.HoiVienID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/NhanVienController.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BTLNHOM15.Models;
+
+namespace BTLNHOM15.Controllers
+{
+    public class NhanVienController : Controller
+    {
+        private readonly MvcGymContext _context;
+
+        public NhanVienController(MvcGymContext context)
+        {
+            _context = context;
+        }
+
+        // GET: NhanVien
+        public async Task<IActionResult> Index()
+        {
+            var mvcGymContext = _context.NhanVien.Include(n => n.ChucVu);
+            return View(await mvcGymContext.ToListAsync());
+        }
+
+        // GET: NhanVien/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.NhanVien == null)
+            {
+                return NotFound();
+            }
+
+            var nhanVien = await _context.NhanVien
+                .Include(n => n.ChucVu)
+                .FirstOrDefaultAsync(m => m.MaID == id);
+            if (nhanVien == null)
+            {
+                return NotFound();
+            }
+
+            return View(nhanVien);
+        }
+
+        // GET: NhanVien/Create
+        public IActionResult Create()
+        {
+            ViewData["MaChucVu"] = new SelectList(_context.Set<ChucVu>(), "MaChucVu", "TenChucVu");
+            return View();
+        }
+
+        // POST: NhanVien/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaID,TenNV,Address,SĐT,EmailNV,MaChucVu")] NhanVien nhanVien)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(nhanVien);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaChucVu"] = new SelectList(_context.Set<ChucVu>(), "MaChucVu", "TenChucVu", nhanVien.MaChucVu);
+            return View(nhanVien);
+        }
+
+        // GET: NhanVien/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.NhanVien == null)
+            {
+                return NotFound();
+            }
+
+            var nhanVien = await _context.NhanVien.FindAsync(id);
+            if (nhanVien == null)
+            {
+                return NotFound();
+            }
+            ViewData["MaChucVu"] = new SelectList(_context.Set<ChucVu>(), "MaChucVu", "TenChucVu", nhanVien.MaChucVu);
+            return View(nhanVien);
+        }
+
+        // POST: NhanVien/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("MaID,TenNV,Address,SĐT,EmailNV,MaChucVu")] NhanVien nhanVien)
+        {
+            if (id != nhanVien.MaID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(nhanVien);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NhanVienExists(nhanVien.MaID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaChucVu"] = new SelectList(_context.Set<ChucVu>(), "MaChucVu", "TenChucVu", nhanVien.MaChucVu);
+            return View(nhanVien);
+        }
+
+        // GET: NhanVien/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.NhanVien == null)
+            {
+                return NotFound();
+            }
+
+            var nhanVien = await _context.NhanVien
+                .Include(n => n.ChucVu)
+                .FirstOrDefaultAsync(m => m.MaID == id);
+            if (nhanVien == null)
+            {
+                return NotFound();
+            }
+
+            return View(nhanVien);
+        }
+
+        // POST: NhanVien/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.NhanVien == null)
+            {
+                return Problem("Entity set 'MvcGymContext.NhanVien'  is null.");
+            }
+            var nhanVien = await _context.NhanVien.FindAsync(id);
+            if (nhanVien != null)
+            {
+                _context.NhanVien.Remove(nhanVien);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NhanVienExists(string id)
+        {
+          return (_context.NhanVien?.Any(e => e.MaID == id)).GetValueOrDefault();
+        }
+    }
+}
